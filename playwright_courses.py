3929ac910db8efa4dcba8e11068a17df0e2b5d72
Artur <autotest-ui@gmail.com>
2026-06-29 01:44:41 +0300

    Работа с авторизацией в Playwright. Практика работы с авторизацией в Playwright

--- playwright_courses.py
+++ playwright_courses.py
@@ -19,14 +19,25 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    context.storage_state(path='browser-state.json')
-
-    dashboard_header = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_header).to_have_text('Dashboard')
+    context.storage_state(path='storage-state.json')
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state='browser-state.json')
+    context = browser.new_context(storage_state='storage-state.json')
     page = context.new_page()
 
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+    courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text('Courses')
+
+    block_icon = page.get_by_test_id('courses-list-empty-view-icon')
+
+    result_block = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(result_block).to_be_visible()
+    expect(result_block).to_have_text('There is no results')
+
+    description_block = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(description_block).to_be_visible()
+    expect(description_block).to_have_text('Results from the load test pipeline will be displayed here')
