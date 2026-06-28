5e7b62ec94cbba2ab041fee2549a64d179b18645
Artur <autotest-ui@gmail.com>
2026-06-28 22:52:35 +0300

    Углубление в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -5,19 +5,19 @@ with sync_playwright() as playwright:
 
     page = browser.new_page()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
     expect(email_input).to_be_visible()
-    email_input.fill("user.name@gmail.com")
+    email_input.fill('user.name@gmail.com')
 
     username_input = page.get_by_test_id('registration-form-username-input').locator('input')
     expect(username_input).to_be_visible()
-    username_input.fill("username")
+    username_input.fill('username')
 
     password_input = page.get_by_test_id('registration-form-password-input').locator('input')
     expect(password_input).to_be_visible()
-    password_input.fill("password")
+    password_input.fill('password')
 
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
